a11ce0034f30f2e9991afed4fa91ff829cc3a859
michaelpipkin <michael.a.pipkin@gmail.com>
2024-08-29 02:08:20 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace KeywordSolverForms
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			label1 = new Label();
+			FirstLetter = new TextBox();
+			SecondLetter = new TextBox();
+			label2 = new Label();
+			FourthLetter = new TextBox();
+			label3 = new Label();
+			ThirdLetter = new TextBox();
+			label4 = new Label();
+			SixthLetter = new TextBox();
+			label5 = new Label();
+			FifthLetter = new TextBox();
+			label6 = new Label();
+			FindWordsButton = new Button();
+			ClearButton = new Button();
+			Answers = new TextBox();
+			StopButton = new Button();
+			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(12, 9);
+			label1.Name = "label1";
+			label1.Size = new Size(59, 15);
+			label1.TabIndex = 0;
+			label1.Text = "First letter";
+			// 
+			// FirstLetter
+			// 
+			FirstLetter.Location = new Point(77, 6);
+			FirstLetter.Name = "FirstLetter";
+			FirstLetter.Size = new Size(100, 23);
+			FirstLetter.TabIndex = 1;
+			FirstLetter.Leave += EnterLetters;
+			// 
+			// SecondLetter
+			// 
+			SecondLetter.Location = new Point(265, 6);
+			SecondLetter.Name = "SecondLetter";
+			SecondLetter.Size = new Size(100, 23);
+			SecondLetter.TabIndex = 3;
+			SecondLetter.Leave += EnterLetters;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(183, 9);
+			label2.Name = "label2";
+			label2.Size = new Size(76, 15);
+			label2.TabIndex = 2;
+			label2.Text = "Second letter";
+			// 
+			// FourthLetter
+			// 
+			FourthLetter.Location = new Point(265, 35);
+			FourthLetter.Name = "FourthLetter";
+			FourthLetter.Size = new Size(100, 23);
+			FourthLetter.TabIndex = 7;
+			FourthLetter.Leave += EnterLetters;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(183, 38);
+			label3.Name = "label3";
+			label3.Size = new Size(72, 15);
+			label3.TabIndex = 6;
+			label3.Text = "Fourth letter";
+			// 
+			// ThirdLetter
+			// 
+			ThirdLetter.Location = new Point(77, 35);
+			ThirdLetter.Name = "ThirdLetter";
+			ThirdLetter.Size = new Size(100, 23);
+			ThirdLetter.TabIndex = 5;
+			ThirdLetter.Leave += EnterLetters;
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new Point(12, 38);
+			label4.Name = "label4";
+			label4.Size = new Size(64, 15);
+			label4.TabIndex = 4;
+			label4.Text = "Third letter";
+			// 
+			// SixthLetter
+			// 
+			SixthLetter.Location = new Point(265, 64);
+			SixthLetter.Name = "SixthLetter";
+			SixthLetter.Size = new Size(100, 23);
+			SixthLetter.TabIndex = 11;
+			SixthLetter.Leave += EnterLetters;
+			// 
+			// label5
+			// 
+			label5.AutoSize = true;
+			label5.Location = new Point(183, 67);
+			label5.Name = "label5";
+			label5.Size = new Size(63, 15);
+			label5.TabIndex = 10;
+			label5.Text = "Sixth letter";
+			// 
+			// FifthLetter
+			// 
+			FifthLetter.Location = new Point(77, 64);
+			FifthLetter.Name = "FifthLetter";
+			FifthLetter.Size = new Size(100, 23);
+			FifthLetter.TabIndex = 9;
+			FifthLetter.Leave += EnterLetters;
+			// 
+			// label6
+			// 
+			label6.AutoSize = true;
+			label6.Location = new Point(12, 67);
+			label6.Name = "label6";
+			label6.Size = new Size(61, 15);
+			label6.TabIndex = 8;
+			label6.Text = "Fifth letter";
+			// 
+			// FindWordsButton
+			// 
+			FindWordsButton.Location = new Point(64, 98);
+			FindWordsButton.Name = "FindWordsButton";
+			FindWordsButton.Size = new Size(75, 23);
+			FindWordsButton.TabIndex = 12;
+			FindWordsButton.Text = "Find Words";
+			FindWordsButton.UseVisualStyleBackColor = true;
+			FindWordsButton.Click += FindWordsButton_ClickAsync;
+			// 
+			// ClearButton
+			// 
+			ClearButton.Location = new Point(145, 98);
+			ClearButton.Name = "ClearButton";
+			ClearButton.Size = new Size(75, 23);
+			ClearButton.TabIndex = 14;
+			ClearButton.Text = "Clear";
+			ClearButton.UseVisualStyleBackColor = true;
+			ClearButton.Click += ClearButton_Click;
+			// 
+			// Answers
+			// 
+			Answers.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			Answers.Location = new Point(18, 127);
+			Answers.Multiline = true;
+			Answers.Name = "Answers";
+			Answers.ReadOnly = true;
+			Answers.ScrollBars = ScrollBars.Vertical;
+			Answers.Size = new Size(347, 222);
+			Answers.TabIndex = 15;
+			// 
+			// StopButton
+			// 
+			StopButton.Enabled = false;
+			StopButton.Location = new Point(226, 98);
+			StopButton.Name = "StopButton";
+			StopButton.Size = new Size(75, 23);
+			StopButton.TabIndex = 16;
+			StopButton.Text = "Stop";
+			StopButton.UseVisualStyleBackColor = true;
+			StopButton.Click += StopButton_Click;
+			// 
+			// Form1
+			// 
+			AcceptButton = FindWordsButton;
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(384, 361);
+			Controls.Add(StopButton);
+			Controls.Add(Answers);
+			Controls.Add(ClearButton);
+			Controls.Add(FindWordsButton);
+			Controls.Add(SixthLetter);
+			Controls.Add(label5);
+			Controls.Add(FifthLetter);
+			Controls.Add(label6);
+			Controls.Add(FourthLetter);
+			Controls.Add(label3);
+			Controls.Add(ThirdLetter);
+			Controls.Add(label4);
+			Controls.Add(SecondLetter);
+			Controls.Add(label2);
+			Controls.Add(FirstLetter);
+			Controls.Add(label1);
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			Name = "Form1";
+			Text = "Keyword Finder";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label label1;
+		private TextBox FirstLetter;
+		private TextBox SecondLetter;
+		private Label label2;
+		private TextBox FourthLetter;
+		private Label label3;
+		private TextBox ThirdLetter;
+		private Label label4;
+		private TextBox SixthLetter;
+		private Label label5;
+		private TextBox FifthLetter;
+		private Label label6;
+		private Button FindWordsButton;
+		private Button ClearButton;
+		private TextBox Answers;
+		private Button StopButton;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+using System.Text;
+using static System.Net.HttpStatusCode;
+
+namespace KeywordSolverForms
+{
+	public partial class Form1 : Form
+	{
+		private CancellationTokenSource _cancellationTokenSource = new();
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void EnterLetters(object sender, EventArgs e) {
+			if (sender is TextBox textBox && textBox.Text.Length > 0) {
+				textBox.Text = string.Concat((textBox.Text).ToUpper().Replace(" ", "").Distinct().OrderBy(c => c));
+			}
+		}
+
+		private async void FindWordsButton_ClickAsync(object sender, EventArgs e) {
+			Answers.Text = string.Empty;
+			if (string.IsNullOrEmpty(FirstLetter.Text) ||
+					string.IsNullOrEmpty(SecondLetter.Text) ||
+					string.IsNullOrEmpty(ThirdLetter.Text) ||
+					string.IsNullOrEmpty(FourthLetter.Text) ||
+					string.IsNullOrEmpty(FifthLetter.Text) ||
+					string.IsNullOrEmpty(SixthLetter.Text)) {
+				Answers.Text = "At least one letter is required in every field.";
+				return;
+			}
+			FindWordsButton.Enabled = false;
+			ClearButton.Enabled = false;
+			StopButton.Enabled = true;
+			Answers.Text = string.Empty;
+			await TestLetters();
+			FindWordsButton.Enabled = true;
+			ClearButton.Enabled = true;
+			StopButton.Enabled = false;
+			FirstLetter.Focus();
+		}
+
+		private void ClearButton_Click(object sender, EventArgs e) {
+			FirstLetter.Text = string.Empty;
+			SecondLetter.Text = string.Empty;
+			ThirdLetter.Text = string.Empty;
+			FourthLetter.Text = string.Empty;
+			FifthLetter.Text = string.Empty;
+			SixthLetter.Text = string.Empty;
+			Answers.Text = string.Empty;
+			FirstLetter.Focus();
+		}
+
+		private void StopButton_Click(object sender, EventArgs e) {
+			_cancellationTokenSource?.Cancel();
+			StopButton.Enabled = false;
+			FindWordsButton.Enabled = true;
+			ClearButton.Enabled = true;
+		}
+
+		private async Task TestLetters() {
+			_cancellationTokenSource = new CancellationTokenSource();
+			var firstLetters = string.Concat(FirstLetter.Text).ToUpper().Replace(" ", "").OrderBy(c => c);
+			var secondLetters = string.Concat(SecondLetter.Text).ToUpper().Replace(" ", "").OrderBy(c => c);
+			var thirdLetters = string.Concat(ThirdLetter.Text).ToUpper().Replace(" ", "").OrderBy(c => c);
+			var fourthLetters = string.Concat(FourthLetter.Text).ToUpper().Replace(" ", "").OrderBy(c => c);
+			var fifthLetters = string.Concat(FifthLetter.Text).ToUpper().Replace(" ", "").OrderBy(c => c);
+			var sixthLetters = string.Concat(SixthLetter.Text).ToUpper().Replace(" ", "").OrderBy(c => c);
+			var answers = new StringBuilder();
+			var http = new HttpClient();
+			await Task.Run(() => {
+				foreach (var firstetter in firstLetters) {
+					foreach (var secondletter in secondLetters) {
+						foreach (var thirdletter in thirdLetters) {
+							foreach (var fourthletter in fourthLetters) {
+								foreach (var fifthletter in fifthLetters) {
+									foreach (var sixthletter in sixthLetters) {
+										if (_cancellationTokenSource.Token.IsCancellationRequested) {
+											Answers.Invoke(new Action(() => Answers.AppendText($"{Environment.NewLine}Search cancelled.")));
+											return;
+										}
+										var possibleWord = $"{firstetter}{secondletter}{thirdletter}{fourthletter}{fifthletter}{sixthletter}";
+										try {
+											var isWord = http.GetAsync($"https://api.dictionaryapi.dev/api/v2/entries/en/{possibleWord.ToLower()}").Result;
+											if (Answers.Text.Length > 0) Answers.Invoke(new Action(() => Answers.AppendText(Environment.NewLine)));
+											switch (isWord.StatusCode) {
+												case OK: {
+														Answers.Invoke(new Action(() => Answers.AppendText($"*** {possibleWord} ***")));
+														return;
+													}
+												case NotFound: {
+														Answers.Invoke(new Action(() => Answers.AppendText(possibleWord)));
+														break;
+													}
+												case TooManyRequests: {
+														Answers.Invoke(new Action(() => Answers.AppendText("Too Many Requests response. Please wait and try again later.")));
+														return;
+													}
+												default: {
+														Answers.Invoke(new Action(() => Answers.AppendText($"Error {isWord.StatusCode}:{isWord.ReasonPhrase}. Please try again.")));
+														return;
+													}
+											}
+										}
+										catch (Exception ex) {
+											Answers.Invoke(new Action(() => Answers.AppendText(ex.Message)));
+											return;
+										}
+										Thread.Sleep(1000);
+									}
+								}
+							}
+						}
+					}
+				}
+				http.Dispose();
+				Answers.Invoke(new Action(() => Answers.AppendText($"{Environment.NewLine}No valid words found.")));
+			});
+		}
+	}
+}
